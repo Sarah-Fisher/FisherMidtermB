08ab8c786a96c9ced6a0c5452114f23b6490c771
Sarah-Fisher <sarah.fisher@mygeorgian.ca>
2023-02-21 16:21:44 -0500

    Updating views

--- FisherMidtermB/Controllers/TacoController.cs
+++ FisherMidtermB/Controllers/TacoController.cs
@@ -54,7 +54,7 @@ namespace FisherMidtermB.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Size,Filling,FirstName,LastName,Phone,DateRequested,Total")] Taco taco)
+        public async Task<IActionResult> Create([Bind("Id,Size,Filling,FirstName,LastName,Phone,DateRequested")] Taco taco)
         {
             if (ModelState.IsValid)
             {
